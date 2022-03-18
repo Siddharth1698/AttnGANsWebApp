82820eb4840914feb18402150f78c5c28813918f
beerus <siddharth.m98@gmail.com>
2022-03-18 22:24:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from multiapp import MultiApp
 import demo
 
 app = MultiApp()
-st.set_page_config(page_title="AttnGAN", initial_sidebar_state="auto")
+st.set_page_config(page_title="GAN Project", initial_sidebar_state="auto")
 
 st.sidebar.title("Navigation")
 # Add all application here
 
-app.add_app("Demo", demo.demo_gan)
-app.add_app("AttnGAN Explanation", demo.attngan_explained)
+app.add_app("Bird GANs", demo.demo_gan)
+
 
 
 # The main app
